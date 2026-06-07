b58cecf5c3ef8fa45575f7e0727ea27b19089a05
Jeong5689 <ojt7470@gmail.com>
2026-06-07 16:33:14 +0900

    fix: 최상위 vite 설정 파일 반영

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/future-compass/', // https://jeong5689.github.io/future-compass/ 👈 여기에 본인의 GitHub 저장소 이름을 슬래시(/) 사이에 꼭 적어주세요!
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
